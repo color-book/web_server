0000086f6b1744002f020bae8acccb09e62bdaed
jetilling <jetilling@hotmail.com>
2018-06-28 21:29:02 -0400

    Refs #1 removed unnecessary code from server file - simple server running

--- server.py
+++ server.py
@@ -1,22 +1,12 @@
 from flask import Flask
-from flask_cors import CORS
-import logging
-import config
-# from flask_wtf.csrf import CSRFProtect
+from flask import jsonify
 
 app = Flask(__name__)
-app.secret_key = config.secret_key
-CORS(app)
-# csrf = CSRFProtect()
-# csrf.init_app(app)
 
-# import login
-import routes
 
-app.config.from_envvar('COLORBOOK_SETTINGS')
-gunicorn_error_logger = logging.getLogger('gunicorn.error')
-app.logger.handlers.extend(gunicorn_error_logger.handlers)
-login.init_app(app)
+@app.route('/')
+def index():
+    return jsonify({'message': 'Hello, World!'})
 
 if __name__ == '__main__':
     app.run()
